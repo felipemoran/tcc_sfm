5a8689d72157b5440c82bd3badbdc7c5fb0cf8d8
Felipe Morán <felipe.moran11@gmail.com>
2020-05-11 19:53:23 -0300

    Bugfix and code refactoring on pipeline 3

--- pipeline/video_pipeline_mk3.py
+++ pipeline/video_pipeline_mk3.py
@@ -13,7 +13,7 @@ class VideoPipelineMK3(BasePipeline):
         self.save_debug_visualization = save_debug_visualization
 
         # Number of frames to skip between used frames (eg. 2 means using frames 1, 4, 7 and dropping 2, 3, 5, 6)
-        self.frames_to_skip = 0
+        self.frames_to_skip = 1
 
         # Number of frames for initial estimation
         self.num_frames_initial_estimation = 10
@@ -25,14 +25,14 @@ class VideoPipelineMK3(BasePipeline):
         self.feature_params = {
             "maxCorners": 200,
             "qualityLevel": 0.5,
-            "minDistance": 30,
+            "minDistance": 15,
             "blockSize": 10
         }
 
         # Parameters for lucas kanade optical flow
         self.lk_params = {
             "winSize": (15, 15),
-            "maxLevel": 2,
+            "maxLevel": 3,
             "criteria": (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
         }
 
@@ -54,95 +54,94 @@ class VideoPipelineMK3(BasePipeline):
         # Start by finding the images
         file, filename = self._get_video(self.dir)
 
-        prev_feature_pack_id = None
-        prev_track_slice = None
-
         Rs = [np.eye(3)]
         Ts = [np.zeros((3, 1))]
-
+        tracks = []
         point_cloud = np.full((self.feature_params['maxCorners'], 3), None, dtype=np.float_)
 
         # Loop through frames (using generators)
+        counter = 0
         for next_track_slice, next_track_slice_index_mask, is_new_feature_set in self._process_next_frame(file):
+            counter += 1
+            tracks += [next_track_slice]
 
             if is_new_feature_set:
-                assert prev_feature_pack_id is None, 'Resetting KLT features is not yet implemented'
-                prev_track_slice = next_track_slice
+                assert len(tracks) == 1, 'Resetting KLT features is not yet implemented'
                 continue
 
-            track_pair = np.array([prev_track_slice, next_track_slice])
+            track_pair = np.array(tracks[-2:])
 
-            point_cloud_mask = (~np.isnan(point_cloud)).any(axis=1)
-            point_cloud_index_mask = np.arange(len(point_cloud))[point_cloud_mask]
-            n_points_in_point_cloud = point_cloud_mask.sum()
+            R, T, new_points, new_point_indexes = self._calculate_pose(track_pair, Rs[-1], Ts[-1], point_cloud)
 
-            # ------ STEP 1 ----------------------------------------------------------------------------------------
-            # calculate R, T_un, points with 2 tracks (5 point + recover pose)
-            R_rel, T_rel, new_points, new_point_indexes = self._get_pose_from_two_tracks(track_pair)
-            # ------ END STEP 1 ------------------------------------------------------------------------------------
+            # if cloud is empty and there are not enough new points, try next frame
+            n_points_in_point_cloud = (~np.isnan(point_cloud)).any(axis=1).sum()
+            if n_points_in_point_cloud == 0 and len(new_points) < self.min_num_points_in_point_cloud:
+                # don't forget to reset track vector and drop old unused data
+                tracks = [tracks[-1]]
+                continue
 
-            assert n_points_in_point_cloud == 0 or n_points_in_point_cloud > self.min_num_points_in_point_cloud, \
-                "Point cloud should have zero points or more than the minimum and not something in between"
+            # merge new points with existing point cloud
+            self._merge_points_into_cloud(point_cloud, new_points, new_point_indexes)
 
-            # if not enough points are in the cloud and track slice at the same time skip refine step
-            if n_points_in_point_cloud > self.min_num_points_in_point_cloud:
-
-                # ------ STEP 2 ----------------------------------------------------------------------------------------
-                # first convert R and T from camera i to 0's perspective
-                R, T = utils.compose_RTs(R_rel, T_rel, Rs[-1], Ts[-1])
-
-                # then convert back to i's perspective but now referencing frame 0 and not frame i-1
-                R = R.transpose()
-                T = np.matmul(R, -T)
-
-                # create new index mask based on existing point cloud's and newly created track's
-                not_nan_mask = (~np.isnan(next_track_slice[point_cloud_index_mask])).any(axis=1)
-
-                # refine R and T based on previous point cloud
-                R, T = self._get_pose_from_points_and_projection(
-                    track_slice=next_track_slice[point_cloud_index_mask][not_nan_mask],
-                    points_3d=point_cloud[point_cloud_index_mask][not_nan_mask],
-                    R=R,
-                    T=T
-                )
-                # Result is in camera 0's coordinate system
-                # ------ END STEP 2 ------------------------------------------------------------------------------------
-
-                # ------ STEP 3 ----------------------------------------------------------------------------------------
-                # get previous R and T and convert them from camera 0's frame to i-1's
-                R_prev = Rs[-1].transpose()
-                T_prev = np.matmul(R_prev, -Ts[-1])
-
-                R_next = R.transpose()
-                T_next = np.matmul(R_next, -T)
-
-                # recalculate points based on refined R and T
-                new_points = self._reproject_tracks_to_3d(R_prev, T_prev, R_next, T_next, track_pair)
-                # ------ END STEP 3 ------------------------------------------------------------------------------------
-            else:
-                R = R_rel
-                T = T_rel
-
-            # if there are enough points in point cloud or new detected points while point cloud is empty
-            # store new camera and point data
-            if (n_points_in_point_cloud == 0 and len(new_points)) > self.min_num_points_in_point_cloud or \
-                    n_points_in_point_cloud > self.min_num_points_in_point_cloud:
-
-                # ------ STEP 4 ----------------------------------------------------------------------------------------
-                # merge new points with existing point cloud
-                # if n_points_in_point_cloud == 0:
-                self._merge_points_into_cloud(point_cloud, new_points, new_point_indexes)
-
-                Rs += [R]
-                Ts += [T]
-                # ------ END STEP --------------------------------------------------------------------------------------
-
-            # prepare everything for next round
-            prev_track_slice = next_track_slice
+            Rs += [R]
+            Ts += [T]
 
         # when all frames are processed, plot result
         utils.write_to_viz_file(self.camera_matrix, Rs, Ts, point_cloud)
         utils.call_viz()
+
+    def _calculate_pose(self, track_pair, prev_R, prev_T, point_cloud):
+        point_cloud_mask = (~np.isnan(point_cloud)).any(axis=1)
+        n_points_in_point_cloud = point_cloud_mask.sum()
+
+        # ------ STEP 1 ----------------------------------------------------------------------------------------
+        # calculate R, T_un, points with 2 tracks (5 point + recover pose)
+        R_rel, T_rel, new_points, new_point_indexes = self._get_pose_from_two_tracks(track_pair)
+        # ------ END STEP 1 ------------------------------------------------------------------------------------
+
+        assert n_points_in_point_cloud == 0 or n_points_in_point_cloud > self.min_num_points_in_point_cloud, \
+            "Point cloud should have zero points or more than the minimum and not something in between"
+
+        # if not enough points are in the cloud and track slice at the same time skip refine step
+        if n_points_in_point_cloud > self.min_num_points_in_point_cloud:
+
+            # ------ STEP 2 ----------------------------------------------------------------------------------------
+            # first convert R and T from camera i to 0's perspective
+            R, T = utils.compose_RTs(R_rel, T_rel, prev_R, prev_T)
+
+            # then convert back to i's perspective but now referencing frame 0 and not frame i-1
+            R = R.transpose()
+            T = np.matmul(R, -T)
+
+            # create new index mask based on existing point cloud's and newly created track's
+            point_cloud_index_mask = np.arange(len(point_cloud))[point_cloud_mask]
+            not_nan_mask = (~np.isnan(track_pair[1][point_cloud_index_mask])).any(axis=1)
+
+            # refine R and T based on previous point cloud
+            R, T = self._get_pose_from_points_and_projection(
+                track_slice=track_pair[1][point_cloud_index_mask][not_nan_mask],
+                points_3d=point_cloud[point_cloud_index_mask][not_nan_mask],
+                R=R,
+                T=T
+            )
+            # Result is in camera 0's coordinate system
+            # ------ END STEP 2 ------------------------------------------------------------------------------------
+
+            # ------ STEP 3 ----------------------------------------------------------------------------------------
+            # recalculate points based on refined R and T
+            new_points = self._reproject_tracks_to_3d(
+                prev_R.transpose(),
+                np.matmul(prev_R.transpose(), -prev_T),
+                R.transpose(),
+                np.matmul(R.transpose(), -T),
+                track_pair)
+            new_point_indexes = np.arange(len(new_points))
+            # ------ END STEP 3 ------------------------------------------------------------------------------------
+        else:
+            R = R_rel
+            T = T_rel
+
+        return R, T, new_points, new_point_indexes
 
     def _merge_points_into_cloud(self, point_cloud, new_points, new_point_indexes):
         # check which points still have no data
@@ -152,9 +151,12 @@ class VideoPipelineMK3(BasePipeline):
         replace_mask = np.intersect1d(nan_mask, new_point_indexes)
         average_mask = np.setdiff1d(new_point_indexes, replace_mask)
 
+        # TODO: there's a bug here!
+
         # for the others, do the average (results in exponential smoothing)
-        point_cloud[replace_mask] = new_points[replace_mask]
-        point_cloud[average_mask] = (point_cloud[average_mask] + new_points[average_mask]) / 2
+        # point_cloud[replace_mask] = new_points[replace_mask]
+        point_cloud[replace_mask] = new_points[[item in replace_mask for item in new_point_indexes]]
+        point_cloud[average_mask] = (point_cloud[average_mask] + new_points[[item in average_mask for item in new_point_indexes]]) / 2
 
 
     # =================== INTERNAL FUNCTIONS ===========================================================================
